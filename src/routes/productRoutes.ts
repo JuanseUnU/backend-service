c84d88c784af2d66c7050ff8fef184b4df7b151c
JuanseUnU <sebasgalviss1415@gmail.com>
2024-11-05 09:57:29 -0500

    ajustes de documentacion

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -8,6 +8,15 @@ import {
 } from "../controllers/productController";
 
 const productRoutes = Router();
+/**
+ * @swagger
+ * /api/products:
+ *  get:
+ *    summary: Obtener todos los productos
+ *    responses:
+ *      200:
+ *        description: Lista de Productos
+ */
 
 productRoutes.get("products/", getAllProducts);
 productRoutes.get("products/:id", getProductByID);
