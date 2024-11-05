1ef9c675b6ae3ac9149b4102c1b2571cd4871f6e
JuanseUnU <sebasgalviss1415@gmail.com>
2024-11-04 19:50:42 -0500

    codigo actualizado

--- src/controllers/productControlles.ts
+++ src/controllers/productControlles.ts
@@ -1,64 +1,116 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { Product } from "../entities/Product";
+// v Trae la tabla/entidad 'Producto' de la base de datos v //
+const productRepository = AppDataSource.getRepository(Product);
+// ^ ___________________________________________________ ^ //
 
-// traemos la tabla o entidad de la base de datos 
-const productRepository = AppDataSource.getRepository(Product)
+// GET Significa | Listar | Traer | Leer |
+// POST Significa | Enviar | Crear |
+// PUT Significa | Actualizar |  
+// DELETE Significa | Remover | Eliminar | 
+//Códigos 200 | Procedimientos exitosos
+//Códigos 400 | No se ha encontrado lo que se busca
+//Códigos 500 | Error de servidor
 
-//obtener todos los productos
-export const getALLProducts = async(req: Request, res: Response) => {
+// v Obtener todos los productos (GET) v //
+export const getAllProducts = async (req: Request, res: Response) => {
+  try { 
+    const products = await productRepository.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({
+        message: "Error al obtener los productos."
+    });
+  }
+ };
+
+// v Obtener un producto (GET) v //
+export const getProductByID = async (req: Request, res: Response) => {
     try {
-        const products = await productRepository.find();
-        res.json(products);
-    } catch(error) {
-      res.status(500).json({
-        message: "Error  al obtener  los productos."
-      });
-    }
-};
-
-//obtner un producto 
-
-export const getALLProductBYID = async(req: Request, res: Response) => {
-    try {
-      const product = await productRepository.findOneBy({
-        id: parseInt(req.params.id)
-      });
-      if (product) {
-        res.json(product);
-      } else {
-        res.status(404).json({
-            message: "Producto  no encontrado."
-        })
-      }
-    } catch(error) {
+        const product = await productRepository.findOneBy({
+            id: parseInt(req.params.id)
+        });
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({
+                message: "Producto no encontrado."
+            });
+        }
+    } catch (error) {
         res.status(500).json({
             message: "Error al obtener el producto."
         });
     }
-};
+ };
 
-// CREAR PRODUCTO
-export const createProduct = async(req: Request, res: Response) => {
+// v Crear un producto (POST) v //
+export const createProduct = async (req: Request, res: Response) => {
     try {
-
-    } catch(error) {
-
+        const {name, description, price} = req.body;
+        const product = new Product();
+        product.name = name;
+        product.description = description;
+        product.price = price;
+        await productRepository.save(product);
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al crear el producto."
+        });
     }
-};
-// actualizar un producto existente 
-export const updateProduct = async(req: Request, res: Response) => {
+ };
+
+  // v Actualizar un producto existente (PUT) v //
+export const updateProduct = async (req: Request, res: Response) => {
     try {
-
-    } catch(error) {
-
+        const{name,description,price} = req.body;
+        // v Buscar producto para actualizar v //
+        const product = await productRepository.findOneBy({
+            id: parseInt(req.params.id)
+        });
+        // v Validar si el producto existe y tenga información v //
+        if (product) {
+            product.name = name ?? product.name;
+            product.description = description ?? product.description;
+            product.price = price ?? product.price;
+            // v Guardamos los cambios del producto v //
+            await productRepository.save(product);
+            res.json(product);
+        } else {
+            res.status(404).json({
+                message: "No se encontró el producto."
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al actualizar el producto."
+        });
     }
-};
-// eliminar un producto 
-export const deleteProduct = async(req: Request, res: Response) => {
+ };
+
+  // v Eliminar un producto existente (DELETE) v //
+export const deleteProduct = async (req: Request, res: Response) => {
     try {
-
-    } catch(error) {
-
+        // v Buscar producto para eliminar v //
+        const product = await productRepository.findOneBy({
+            id: parseInt(req.params.id)
+        });
+        // v Validar si el producto existe y tenga información v //
+        if (product) {
+            await productRepository.remove(product);
+            res.json({
+                message: "El producto ha sido eliminado."
+            });
+        } else {
+            res.status(404).json({
+                message: "No se encontró el producto."
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al eliminar el producto."
+        });
     }
-};
+ };
